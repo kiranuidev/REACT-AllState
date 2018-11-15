f6c56d355a6415115b81fa06d7b0a87778bac76d
kiran.paturi <kiran>
2018-11-15 12:13:02 +0530

    Added Header and Footer

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar';
+import Footer from './components/footer';
 
 class App extends Component {
+   constructor(){
+      super();
+      this.configureHeader = {
+          companyName:'All State'
+      }
+      this.configureFooter ={
+        footerData:["About us", 
+        "Contact Us", "Site Map",
+        "All State Blog",
+        "Careers",
+        "All State Investments"]
+      }
+   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <NavBar  title={this.configureHeader.companyName} />
+        <Footer  
+        footerItems =
+         {this.configureFooter.footerData}
+          />
       </div>
+     
     );
   }
 }
